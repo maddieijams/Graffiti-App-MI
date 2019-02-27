35836b15dd433a926e16d0cf76bcc3ae8935feaa
Maddie Ijams <maddieijams@gmail.com>
2019-02-27 15:06:22 -0500

    g

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('user', {
-        username: DataTypes.STRING,
-        password: DataTypes.STRING
+        username: { type: DataTypes.STRING,
+                    allowNull: false,
+                    unique: true
+                },
+        password: { type: DataTypes.STRING,
+                    allowNull: false,
+                    unique: true
+                }
     });
 }
