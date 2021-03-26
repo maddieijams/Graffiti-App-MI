1f7166725a16f2a7cf5e2cc8ae908089c3b958fa
Maddie Ijams <maddieijams@gmail.com>
2021-03-25 20:14:04 -0400

    op fix

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  operatorsAliases: false,
+  operatorsAliases: Sequelize.Op,
 });
 
 sequelize.authenticate().then(
