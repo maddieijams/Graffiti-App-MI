30cd223e03f3f7ab45c94b0e9b683a00b9633aab
Maddie Ijams <maddieijams@gmail.com>
2021-03-28 12:20:52 -0400

    change port number

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,20 +1,20 @@
-// module.exports = (req, res, next) => {
-//   res.header("access-control-allow-origin", "*");
-//   res.header("access-control-allow-methods", "POST, DELETE, PUT, GET, OPTIONS");
-//   res.header(
-//     "access-control-allow-headers",
-//     "Origin, X-Requested-With, Content-Type, Authorization"
-//   );
-//   next();
-// };
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+module.exports = (req, res, next) => {
+  res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "POST, DELETE, PUT, GET, OPTIONS");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "access-control-allow-headers",
+    "Origin, X-Requested-With, Content-Type, Authorization"
   );
   next();
-});
+};
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+//   );
+//   next();
+// });
--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,18 +1,19 @@
-let APIURL = '';
+let APIURL = "";
 
 switch (window.location.hostname) {
-    // this is the local host name of your react app
-    case 'localhost' || '127.0.0.1':
-        // this is the local host name of your API
-        APIURL = 'http://localhost:3000';
-        break;
+  // this is the local host name of your react app
+  case "localhost" || "127.0.0.1":
+    // this is the local host name of your API
+    APIURL = "http://localhost:8000";
+    break;
 
-    // this is the deployed react application
-    case 'graffiti-app-client.herokuapp.com':
-        // this is the full url of your deployed API
-        APIURL = 'https://graffiti-app-mi.herokuapp.com'
-        break;
-        default: break;
+  // this is the deployed react application
+  case "graffiti-app-client.herokuapp.com":
+    // this is the full url of your deployed API
+    APIURL = "https://graffiti-app-mi.herokuapp.com";
+    break;
+  default:
+    break;
 }
 
 export default APIURL;
