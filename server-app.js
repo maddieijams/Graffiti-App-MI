465e77584a32959dfbc15a96be63bff3b49b3a67
Maddie Ijams <maddieijams@gmail.com>
2021-04-15 21:46:45 -0400

    maybe?

--- server-app.js
+++ server-app.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "build")));
 app.use(require("./middleware/headers"));
-app.use("/", express.static(__dirname + "public"));
+// app.use("/", express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //modal, login or signup
 app.use("", home);
