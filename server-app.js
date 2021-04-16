b9991479e1b3e36eb8076156a70478f804b07f0e
Maddie Ijams <maddieijams@gmail.com>
2021-04-15 22:21:50 -0400

    meh

--- server-app.js
+++ server-app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./middleware/headers"));
 // app.use("/", express.static(__dirname + "public"));
-app.use(express.static(__dirname + "/public"));
+app.use("/", express.static(__dirname + "/public"));
 
 //modal, login or signup
 app.use("", home);
